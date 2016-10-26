4c0281e4e0d4179b6fc303b8ec4ec6fe6f6614e1
Joshua Soberg <j15soberg@gmail.com>
2016-10-26 19:54:39 -0400

    - Messed around with the perlin noise to generate a much more cohesive terrain.

--- Assets/GameScene/Scripts/TerrainHeightmapGenerator.cs
+++ Assets/GameScene/Scripts/TerrainHeightmapGenerator.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class TerrainHeightmapGenerator : MonoBehaviour
 {
-
     void Start ()
     {
-        GenerateHeightmap(UnityEngine.Random.Range(0f, 1f));
+        GenerateHeightmap();
     }
 
-    private void GenerateHeightmap(float effector)
+    private void GenerateHeightmap()
     {
         TerrainData TD = GetComponent<Terrain>().terrainData;
         float[,] HeightMap = new float[TD.heightmapWidth, TD.heightmapWidth];
@@ -18,14 +17,15 @@ public class TerrainHeightmapGenerator : MonoBehaviour
         {
             for (int y = 0; y < TD.heightmapHeight; y++)
             {
-                HeightMap[x, y] = Mathf.PerlinNoise((float) x / (float) TD.heightmapWidth, (float) y / (float)TD.heightmapHeight);
+                float perlinX = ((float)x / (float)TD.heightmapWidth) * 10f;
+                float perlinY = ((float)y / (float)TD.heightmapHeight) * 10f;
+                float noise = Mathf.PerlinNoise(perlinX, perlinY);
+                HeightMap[x, y] = noise;
             }
         }
 
         TD.SetHeights(0, 0, HeightMap);
     }
-
-    private Vector3 CurrentMainCamTransform;
 
 	void Update ()
     {
