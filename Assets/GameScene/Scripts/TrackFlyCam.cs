374ec1e0163c4f3bba10007095e9e0e3418c9cd9
Joshua Soberg <j15soberg@gmail.com>
2016-11-28 20:17:02 -0500

    - Added a basic track cam. Readjusted the terrains and camera to have some semblance of a game.

---
+++ Assets/GameScene/Scripts/TrackFlyCam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackFlyCam : MonoBehaviour
+{
+    public float BaseMovementSpeed;
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        transform.position += transform.forward * (BaseMovementSpeed) * Time.deltaTime;
+    }
+}
