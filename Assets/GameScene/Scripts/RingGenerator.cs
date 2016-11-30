31399a1a9c40edb9672248f70fbf74e34b394bc2
Joshua Soberg <j15soberg@gmail.com>
2016-11-29 20:34:48 -0500

    - Added foundation ring generator script.

---
+++ Assets/GameScene/Scripts/RingGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingGenerator : MonoBehaviour
+{
+    private const float ValueAheadOfMainCharacterToPlaceRing = 10000;
+
+    public GameObject MainCharacter;
+    public GameObject RingPrefab;
+
+    public float BaseSecondsToGenerateNewRing;
+
+    private float TimeSinceLastGenerate;
+
+	void Update ()
+    {
+        TimeSinceLastGenerate += Time.deltaTime;
+        if (ShouldGenerateRing()) {
+            GenerateRingForMainCharacter();
+            TimeSinceLastGenerate = 0f;
+        }
+	}
+
+    private bool ShouldGenerateRing()
+    {
+        return (TimeSinceLastGenerate > BaseSecondsToGenerateNewRing);
+    }
+
+    private void GenerateRingForMainCharacter()
+    {
+        Vector3 currentPosition = MainCharacter.transform.position;
+        // TODO put ring somewhere to the left/right of character so they have to move to it.
+        currentPosition.x += ValueAheadOfMainCharacterToPlaceRing;
+
+        Instantiate(RingPrefab, currentPosition, Quaternion.identity);
+    }
+}
