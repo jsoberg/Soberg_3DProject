ec37878538cf54d1f67f3fb13df9e419407f00f9
Joshua Soberg <j15soberg@gmail.com>
2016-12-03 16:09:13 -0500

    - Decreased the size of terrains/water/camera distance in an attempt to decrease lag. Minimizing things seemed to help. Readjusted terrain generation to give more interesting terrain. Implemented a track cam that will automatically move upward based on the terrain height underneath it.

---
+++ Assets/GameScene/Scripts/ActiveTerrainSetter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveTerrainSetter : MonoBehaviour
+{
+    public Terrain TerrainOne;
+    public Terrain TerrainTwo;
+
+    public Terrain ActiveTerrain { get; internal set; }
+
+    public ActiveTerrainSetter()
+    {
+        ActiveTerrain = TerrainOne;
+    }
+
+    public float MaxDistance;
+
+    void Update ()
+    {
+        Ray ray = new Ray(Camera.main.transform.position, new Vector3(0, -MaxDistance, 0));
+        RaycastHit hit;
+        if (TerrainOne.GetComponent<Collider>().Raycast(ray, out hit, MaxDistance)) {
+            ActiveTerrain = TerrainOne;
+        } else {
+            ActiveTerrain = TerrainTwo;
+        }
+	}
+}
--- Assets/GameScene/Scripts/TrackFlyCam.cs
+++ Assets/GameScene/Scripts/TrackFlyCam.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class TrackFlyCam : MonoBehaviour
 {
     public float BaseMovementSpeed;
+    public float HeightAboveTerrain;
+    public ActiveTerrainSetter ActiveTerrainSetter;
 
     void Start ()
     {
@@ -12,6 +14,11 @@ public class TrackFlyCam : MonoBehaviour
 	
 	void Update ()
     {
-        transform.position += transform.forward * (BaseMovementSpeed) * Time.deltaTime;
+        float xForward = transform.forward.x * (BaseMovementSpeed) * Time.deltaTime;
+        transform.position += new Vector3(xForward, 0, 0);
+
+        float height = ActiveTerrainSetter.ActiveTerrain.SampleHeight(transform.position);
+        float y = Mathf.Lerp(transform.position.y, height + HeightAboveTerrain, (float) 0.08);
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
     }
 }
