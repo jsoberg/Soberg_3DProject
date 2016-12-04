108279eb3d24b68df7f1ae414013d856f8207af8
Joshua Soberg <j15soberg@gmail.com>
2016-12-04 13:23:28 -0500

    - Modified the old Perlin heightmap generator to work with the new method of heightmap generator, as I describe the Perlin generation in the project paper so wanted to have a usable reference.

---
+++ Assets/GameScene/Scripts/HeightmapGeneration/PerlinHeightmapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerlinHeightmapGenerator : HeightmapGenerator
+{
+    public float Factor;
+
+    public override float[,] GenerateHeightMap(int width, int height, int max)
+    {
+        float[,] heightMap = new float[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                float perlinX = ((float)x / (float) width) * Factor;
+                float perlinY = ((float)y / (float) height) * Factor;
+                float noise = Mathf.PerlinNoise(perlinX, perlinY);
+                heightMap[x, y] = noise;
+            }
+        }
+
+        return heightMap;
+    }
+}
