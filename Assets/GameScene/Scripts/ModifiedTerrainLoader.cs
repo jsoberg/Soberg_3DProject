e25192b57bc2e05d2c8c075f640b1e2e1c37a174
Joshua Soberg <j15soberg@gmail.com>
2016-12-08 16:11:17 -0500

    - Pushing work done on Tuesday.

---
+++ Assets/GameScene/Scripts/ModifiedTerrainLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifiedTerrainLoader : MonoBehaviour {
+
+    public Terrain Terrain;
+
+    public HeightmapGenerator HeightmapGenerator;
+    public TextureAlphaGenerator TextureAlphaGenerator;
+
+    void Start()
+    {
+        GenerateInitial(Terrain.terrainData);
+    }
+
+    private void GenerateInitial(TerrainData td)
+    {
+        int width = td.heightmapWidth;
+        int height = td.heightmapHeight;
+        int max = (int)td.size.y;
+
+        float[,] heightmap = HeightmapGenerator.GenerateHeightMap(width, height, max);
+        float[,,] alphaMap = TextureAlphaGenerator.GenerateTextureAlphas(heightmap, max, td.alphamapWidth, td.alphamapHeight);
+        td.SetHeights(0, 0, heightmap);
+        td.SetAlphamaps(0, 0, alphaMap);
+    }
+}
